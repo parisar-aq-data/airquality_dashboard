a3ecb5ce647f6fabd2856dcb1bf6290530ff5ef2
shwetapurushe <shwetapurushe@gmail.com>
2022-04-17 11:20:40 -0400

    new App.js and cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+Controls the side control panel and the viz Panel 
+*/
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import VizPanel from "./visualizationPanel/VizPanel.js";
+import logo from "./assets/ParisarLogo.png";
+
+import { Nav, Navbar, Container } from "react-bootstrap";
+import ControlPanel from "./controlPanel/ControlPanel";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMode: "IUDX",
+    };
+  }
+
+  setSelectedMode = (e) => {
+    console.log(
+      "Calling handleselectedMode in App.js and Setting value to ",
+      e.target.value
+    );
+    this.setState({
+      selectedMode: e.target.value,
+    });
+  };
+
+  render() {
+    const content = (
+      <>
+        <ControlPanel setSelectedMode={this.setSelectedMode} />
+        <VizPanel selectedMode={this.state.selectedMode} />
+      </>
+    );
+
+    return (
+      <div className="parentdiv">
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img alt="" src={logo} className="logo" /> Air Quality Dashboard
+            </Navbar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav.Link href="#home">About</Nav.Link>
+              <Nav.Link href="#features">Contact Us</Nav.Link>
+              <Nav.Link href="#pricing">Analysis</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <div className="content">{content}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
