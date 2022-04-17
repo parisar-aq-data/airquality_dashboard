913f0fcf058d34a2c56fae51df8dc629fdd9f668
shwetapurushe <shwetapurushe@gmail.com>
2022-04-17 11:27:32 -0400

    barchart horizontal tool

---
+++ src/visualizationPanel/vizTools/BarchartTool_Horizontal.js
@@ -0,0 +1,82 @@
+import * as d3 from "d3";
+import React, { useEffect, useRef } from "react";
+
+export default function BarchartToolHorizontal(props) {
+  const { width, height } = props;
+  const svgRef = useRef(null);
+
+  const svgWidth = width;
+  const svgHeight = height;
+  const margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 100,
+  };
+
+  // DATA to populate CHART
+  const data = props.rankedWards;
+
+  useEffect(() => {
+    const svgEl = d3.select(svgRef.current);
+    const h = svgHeight + 20;
+
+    // Y
+    const y_scale = d3
+      .scaleBand()
+      .domain(
+        data.map(function (d) {
+          return d.name;
+        })
+      )
+      .range([svgHeight, 0])
+      .padding(0.2);
+
+    // X
+    const x_scale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => Number(d.Average_pm25))])
+      .nice()
+      .range([0, svgWidth - margin.right]);
+
+    //ViewBOX
+    svgEl.attr("viewBox", "-45 0" + " " + svgWidth + " " + h);
+
+    if (svgHeight > 0) {
+      /* X axis */
+      svgEl
+        .append("g")
+        .call(d3.axisBottom(x_scale))
+        .attr("transform", "translate(0," + svgHeight + ")"); // -- for flipping top to bottom
+
+      /* Y axis */
+      svgEl.append("g").call(d3.axisLeft(y_scale));
+
+      svgEl
+        .selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("height", function (d) {
+          return y_scale.bandwidth();
+        })
+        .attr("y", function (d) {
+          return y_scale(d.name);
+        })
+        .attr("width", function (d) {
+          return x_scale(Number(d.Average_pm25));
+        })
+        .attr("fill", function (d) {
+          return d.best_rank < 4 ? "#9ad8f5" : "#eab07b";
+        });
+    }
+  });
+
+  return (
+    <div>
+      <div className="vizTitle">{props.title}</div>
+      <svg width={width} height={height} ref={svgRef}></svg>
+    </div>
+  );
+}
