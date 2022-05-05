4df23ea535675546e403f106fb0dc46071465e3c
shwetapurushe <shwetapurushe@gmail.com>
2022-05-05 18:57:46 -0400

    formatting

--- src/visualizationPanel/SVGContainer.js
+++ src/visualizationPanel/SVGContainer.js
@@ -1,28 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
- export default function SVGContainer (props) {
+export default function SVGContainer(props) {
+  const { children } = props;
 
-    const { children } = props
+  const [containerWidth, setWidth] = useState(0);
+  const [containerHeight, setHeight] = useState(0);
 
-    const [containerWidth, setWidth] = useState(0)
-    const [containerHeight, setHeight] = useState(0)
+  const containerRef = useRef(); //memory address containerRef.current
 
-    const containerRef = useRef() //memory address containerRef.current 
+  useEffect(() => {
+    if (containerRef.current) {
+      setWidth(containerRef.current.offsetWidth);
+      setHeight(containerRef.current.offsetHeight);
+    }
+  }, []); // if 2 changes call 1
 
-    useEffect(() => {
-        if (containerRef.current) {
-            setWidth(containerRef.current.offsetWidth)
-            setHeight(containerRef.current.offsetHeight)
-        }
-    }, [])// if 2 changes call 1 
-
-    const clonedChild = React.cloneElement(children, { width: containerWidth, height: containerHeight })
-    return (
-        <div
-             className={`svgcontainer ${props.styleName}`} ref={containerRef}
-        >
-            {clonedChild}
-            
-        </div>
-    )
+  const clonedChild = React.cloneElement(children, {
+    width: containerWidth,
+    height: containerHeight,
+  });
+  return (
+    <div className={`svgcontainer ${props.styleName}`} ref={containerRef}>
+      {clonedChild}
+    </div>
+  );
 }
