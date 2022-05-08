74264999ca5accb7daa550161ea4ac9ff0439fd1
shwetapurushe <shwetapurushe@gmail.com>
2022-05-08 11:36:51 -0400

    panCityView vs monitorView sharing Polygons -- WORKING

--- src/visualizationPanel/vizTools/ReactMapTool.js
+++ src/visualizationPanel/vizTools/ReactMapTool.js
@@ -6,8 +6,6 @@ import {
   CircleMarker,
   Popup,
   LayersControl,
-  useMapEvent,
-  useMapEvents,
   useMap,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
@@ -16,72 +14,31 @@ function MonitorView(props) {
   const map = useMap();
 
   // finding the matching shape data for the matched feature
-  let matchedFeature = props.shapes.features.find(
-    (feat) => feat.properties.name_mr == props.selectedWardOrMonitor
+  let matchedFeature = props.wardsAndMonitors.find(
+    (feat) => feat.name === props.selectedWardOrMonitor
   );
 
-  console.log("matched polygon", matchedFeature);
+  console.log("matched feature", matchedFeature);
 
   let [centerX, centerY] = [
-    parseFloat(matchedFeature.properties.lat),
-    parseFloat(matchedFeature.properties.lon),
+    parseFloat(matchedFeature.lat),
+    parseFloat(matchedFeature.lon),
   ];
 
   map.setView([centerX, centerY], 14);
 
-  const polygons = [];
-  polygons.push(
-    <Polygon
-      key={matchedFeature.properties.name_mr}
-      pathOptions={{
-        color: "pink",
-        fillOpacity: 0.7,
-      }}
-      positions={matchedFeature.geometry.coordinates[0]}
-    ></Polygon>
-  );
-
-  return polygons;
+  return null;
 }
 
 function PanCityView(props) {
   const map = useMap();
   map.setView([18.502, 73.853], 12);
-
-  function colorMapper(d) {
-    if (!d) return "#9c9c9c";
-    return d > 30
-      ? "#994C01"
-      : d > 25
-      ? "#C89866"
-      : d > 23
-      ? "#C9E3E7"
-      : d > 20
-      ? "#AAD9E6"
-      : d > 13
-      ? "#80C6E6"
-      : "#80efff";
-  }
-
-  const polygons = [];
-  props.features.forEach((feat, index) => {
-    polygons.push(
-      <Polygon
-        key={index}
-        pathOptions={{
-          color: "#0E86D4",
-          fillColor: colorMapper(feat.properties.average_daily_pm25),
-          fillOpacity: 0.7,
-        }}
-        positions={feat.geometry.coordinates[0]}
-      ></Polygon>
-    );
-  });
-  return polygons;
+  return null;
 }
 
 export default function ReactMapTool(props) {
   let features = [];
+  let polygons = [];
 
   /* WARD POLYGONS */
   if (props.shapes) {
@@ -95,20 +52,34 @@ export default function ReactMapTool(props) {
       }
     }
 
-    // features.forEach((feat, index) => {
-    //   // console.log("feat", feat);
-    //   polygons.push(
-    //     <Polygon
-    //       key={index}
-    //       pathOptions={{
-    //         color: "#0E86D4",
-    //         fillColor: colorMapper(feat.properties.average_daily_pm25),
-    //         fillOpacity: 0.7,
-    //       }}
-    //       positions={feat.geometry.coordinates[0]}
-    //     ></Polygon>
-    //   );
-    // });
+    function colorMapper(d) {
+      if (!d) return "#9c9c9c";
+      return d > 30
+        ? "#994C01"
+        : d > 25
+        ? "#C89866"
+        : d > 23
+        ? "#C9E3E7"
+        : d > 20
+        ? "#AAD9E6"
+        : d > 13
+        ? "#80C6E6"
+        : "#80efff";
+    }
+
+    features.forEach((feat, index) => {
+      polygons.push(
+        <Polygon
+          key={index}
+          pathOptions={{
+            color: "#0E86D4",
+            fillColor: colorMapper(feat.properties.average_daily_pm25),
+            fillOpacity: 0.7,
+          }}
+          positions={feat.geometry.coordinates[0]}
+        ></Polygon>
+      );
+    });
   }
 
   return (
@@ -119,13 +90,13 @@ export default function ReactMapTool(props) {
         <MonitorView
           wardsAndMonitors={props.monitors}
           selectedWardOrMonitor={props.selectedWardOrMonitor}
-          shapes={props.shapes}
         />
       )}
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {polygons}
     </MapContainer>
   );
 }
