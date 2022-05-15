f456e4ef51e2242e521a1c7ef4c6d2310da9177a
shwetapurushe <shwetapurushe@gmail.com>
2022-05-15 10:29:24 -0400

    reusing, wrap function for long labels

--- src/visualizationPanel/vizTools/BarchartTool.js
+++ src/visualizationPanel/vizTools/BarchartTool.js
@@ -3,7 +3,6 @@ import React, { useEffect, useRef } from "react";
 
 export default function BarchartTool(props) {
   const { width, height } = props;
-  console.log("Props in barchart tool", props);
   const svgRef = useRef(null);
 
   const svgWidth = width;
@@ -15,10 +14,49 @@ export default function BarchartTool(props) {
     left: 10,
   };
 
-  const data = props.rankedWards;
+  let data = props.rankedWards;
+  //SORTING BY RANK
+  data = data.sort((a, b) => a.best - b.best);
+
+  //REF: https://bl.ocks.org/guypursey/f47d8cd11a8ff24854305505dbbd8c07
+  function wrap(text, width) {
+    console.log("calling wrap");
+    text.each(function () {
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text
+          .text(null)
+          .append("tspan")
+          .attr("x", 0)
+          .attr("y", y)
+          .attr("dy", dy + "em");
+      while ((word = words.pop())) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text
+            .append("tspan")
+            .attr("x", 0)
+            .attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+            .text(word);
+        }
+      }
+    });
+  }
 
   useEffect(() => {
     const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove();
     const h = svgHeight + 20;
 
     const x_scale = d3
@@ -40,18 +78,21 @@ export default function BarchartTool(props) {
     //ViewBOX
     svgEl.attr("viewBox", "-30 10" + " " + svgWidth + " " + h);
 
+    let xAxis = d3.axisBottom(x_scale);
+
     if (svgHeight > 0) {
       /* X axis */
       svgEl
         .append("g")
-        .call(d3.axisBottom(x_scale))
+        .attr("class", "x axis")
         .attr("transform", "translate(0," + svgHeight + ")") // -- for flipping top to bottom
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+        .call(xAxis)
+        .selectAll(".tick text")
+        .call(wrap, x_scale.bandwidth());
 
       /* Y axis */
-      svgEl.append("g").call(d3.axisLeft(y_scale));
+      let yAxis = d3.axisLeft(y_scale);
+      svgEl.append("g").call(yAxis);
 
       svgEl
         .selectAll("mybar")
@@ -68,9 +109,16 @@ export default function BarchartTool(props) {
         .attr("height", function (d) {
           return svgHeight - y_scale(Number(d.Average_pm25));
         })
-        .attr("fill", "#9ad8f5");
+        .attr("fill", function (d) {
+          return d.best < 4 ? "#9ad8f5" : "#FCC782";
+        });
     }
   });
 
-  return <svg width={width} height={height} ref={svgRef}></svg>;
+  return (
+    <div>
+      <div className="vizTitle">{props.title}</div>
+      <svg width={width} height={height} ref={svgRef}></svg>
+    </div>
+  );
 }
