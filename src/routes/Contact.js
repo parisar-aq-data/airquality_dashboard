a63d76ab97e3e66d424495775f7656dca5698907
shwetapurushe <shwetapurushe@gmail.com>
2022-06-21 18:08:05 -0400

    font awesome for social media icons

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
+
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 export const Contact = () => {
   const [lgShow, setLgShow] = useState(false);
+  library.add(faInstagram, faFacebook, faTwitter);
 
   return (
     <>
@@ -21,10 +32,46 @@ export const Contact = () => {
         onHide={() => setLgShow(false)}
         aria-labelledby="example-modal-sizes-title-lg"
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-modal-sizes-title-lg">
+        <Modal.Header closeButton style={{ display: "flex" }}>
+          <Modal.Title id="example-modal-sizes-title-lg" style={{ flex: 1 }}>
             Reach out to us
           </Modal.Title>
+          <span>
+            <a
+              href="https://www.instagram.com/parisar_org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                size={"xl"}
+                icon="fa-brands fa-instagram"
+                fixedWidth
+              />
+            </a>
+            <a
+              href="https://twitter.com/parisarpune"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                size={"xl"}
+                icon="fa-brands fa-twitter"
+                fixedWidth
+              />
+            </a>
+            <a
+              href="https://www.facebook.com/ParisarUrbanTransport/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                size={"xl"}
+                icon="fa-brands fa-facebook"
+                fixedWidth
+                className="facebook"
+              />
+            </a>
+          </span>
         </Modal.Header>
         <Modal.Body>
           Please send us your questions{" "}
